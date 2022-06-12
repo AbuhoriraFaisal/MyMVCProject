8dbe029363efb98e59b274a03deee94dcd4a9581
Abuhorira Faisal <abuhorira91011@gmail.com>
2022-06-12 13:36:03 +0200

    Add project files.

---
+++ Core/Entities/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    internal class Category
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        [Display(Name ="Category")]
+        public string Name { get; set; }
+        [Required]
+        public string Decription { get; set; }
+        public DateTime OperationDate { get; set; } = DateTime.Now;
+    }
+}
